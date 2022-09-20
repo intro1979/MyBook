1702e93b73e49905c0eb8198a99a59a8cb1c5bb5
intro1979 <46369909+intro1979@users.noreply.github.com>
2022-09-20 16:16:20 +0200

    2 files modified

--- FBcrud/src/app/components/editbook/editbook.component.ts
+++ FBcrud/src/app/components/editbook/editbook.component.ts
@@ -8,7 +8,7 @@ import { MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
   styleUrls: ['./editbook.component.scss']
 })
 export class EditbookComponent implements OnInit {
-  @Input() id!: string;
+  @Input() id: string;
   book!: IBook;
 
   constructor(
